e60faf167a8f0af9684aecbede385bdd6bef53bf
mei <meimeipuppy@gmail.com>
2023-03-23 14:36:43 +0800

    feat:change sidebar name

---
+++ src/components/SideNavigation.vue
@@ -0,0 +1,33 @@
+<template>
+    <!-- <a-card 
+    style="width: 80%; margin: auto;  box-shadow: 0px 20px 27px rgba(0, 0, 0, 0.05)"
+    :bordered="false"
+    
+    >
+    <customer-service-filled />
+    <a-typography-text size="small">Customer Service
+    </a-typography-text>
+    </a-card> -->
+    
+    
+    <a-button type="text">
+        <customer-service-filled />
+        Customer Service
+    </a-button>
+</template>
+
+<script>
+import { defineComponent, /*reactive*/ } from 'vue';
+import { CustomerServiceFilled } from '@ant-design/icons-vue';
+export default defineComponent({
+  components: {
+    CustomerServiceFilled
+  },
+  setup() {
+    
+    return {
+      
+    };
+  },
+});
+</script>
